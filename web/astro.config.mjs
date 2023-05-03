5d12a538b51a3d1f3659c01ebf080d32b898f2fa
dingusn10 <87838062+dingusn10@users.noreply.github.com>
2023-05-03 18:17:24 +0000

    installed astro integrations: react, tailwind, vercel, sitemap, robots.txt, and images.

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
+import image from "@astrojs/image";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), image(), tailwind(), sitemap(), robotsTxt()],
+  output: "server",
+  adapter: vercel()
+});
