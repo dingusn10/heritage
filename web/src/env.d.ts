f694e37abd717b034ff91d0a3c9547924729bedb
dingusn10 <87838062+dingusn10@users.noreply.github.com>
2023-05-04 17:40:05 +0000

    still messing with API call

--- web/src/env.d.ts
+++ web/src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+  readonly PUBLIC_PCO_ID: string;
+  readonly PUBLIC_PCO_KEY: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
