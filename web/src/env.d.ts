086601e70b61ec2078045786a4f24da1b05839b2
dingusn10 <87838062+dingusn10@users.noreply.github.com>
2023-05-15 14:41:00 +0000

    moving to next

--- web/src/env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="astro/client" />
-interface ImportMetaEnv {
-  readonly PUBLIC_PCO_KEY: string;
-  // more env variables...
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
