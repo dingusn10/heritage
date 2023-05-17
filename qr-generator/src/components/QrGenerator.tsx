7c0fee6a909c88d4748bfe1c4a4c952e9b0ebe38
dingusn10 <87838062+dingusn10@users.noreply.github.com>
2023-05-17 20:40:23 +0000

    skeleton of qr generator page

--- qr-generator/src/components/QrGenerator.tsx
+++ qr-generator/src/components/QrGenerator.tsx
@@ -4,6 +4,19 @@ import { QRCodeSVG } from "qrcode.react"
 
 export default function QrGenerator() {
   return (
-    <div>QrGenerator</div>
+    <> 
+        <div className="">
+            <form action="">
+                {
+                    //Create new QR code.
+                }
+            </form>
+            <div>
+                {
+                    //Display QR Codes here.
+                }
+            </div>
+        </div>
+    </>
   )
 }
