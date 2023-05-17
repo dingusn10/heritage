c66322f5171282a0900faacfda9eb75e8bafc6ce
dingusn10 <87838062+dingusn10@users.noreply.github.com>
2023-05-17 20:37:25 +0000

    added qrRouter to api router root

--- qr-generator/src/server/api/root.ts
+++ qr-generator/src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { exampleRouter } from "~/server/api/routers/qr";
+import { qrRouter } from "~/server/api/routers/qr";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { exampleRouter } from "~/server/api/routers/qr";
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
+  qrCode: qrRouter,
 });
 
 // export type definition of API
